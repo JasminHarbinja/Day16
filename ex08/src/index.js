cb6a2a3db777b97ab3bcb600a179e1216460115a
Jasmin Harbinja <57997257+JasminHarbinja@users.noreply.github.com>
2021-03-23 22:15:47 +0100

    Add files via upload

---
+++ ex08/src/index.js
@@ -0,0 +1,16 @@
+class Person {
+    constructor(name, email, age) {
+        this.Name = name;
+        this.email = email;
+        this.age = age;
+    }
+    getPerson() {
+        let personInfo = {
+            Name: this.Name,
+            email: this.email,
+            age: this.age
+        }
+        return personInfo;
+    }
+}
+module.exports = Person;
